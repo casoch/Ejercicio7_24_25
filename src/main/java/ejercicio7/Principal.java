f8bea88fc11ca81b0ffcbb24d94bac5f3b53c462
casoch <casoch@gmail.com>
2025-04-23 15:53:42 +0200

    generar primer ejecutable

---
+++ src/main/java/ejercicio7/Principal.java
@@ -0,0 +1,13 @@
+package ejercicio7;
+
+import ejercicio7.Atraccion.Tipo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Atraccion a = new Atraccion("Dragon Khan", Tipo.MONTAÑA_RUSA, 100);
+		System.out.println(a);
+
+	}
+
+}
